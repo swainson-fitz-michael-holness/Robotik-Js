e83bcc07f1923ea14f60e815c0484f40ec7747f0
Swainson Holness <swainson.holness@gmail.com>
2017-06-26 22:52:33 -0600

    Update word-count.js

--- main-algorithms/word-count/word-count.js
+++ main-algorithms/word-count/word-count.js
@@ -7,3 +7,33 @@ function word_count(str) {
   var output = "Word count: "+ sum + ", Unique Count: "+ count_arr.length
   return output;
 }
+
+function wrd_freq(str) {
+  //define variables
+  var holding_arr = [];
+  var update = str.replace(/'/g, "").replace(/\W/g, " ");
+  var arr = update.split(" ");
+   
+  //split words into an array
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "") {
+      arr.splice(i, 1);
+    }
+  }  
+   
+  //create an aray with unique unique words and their frequency in paragraph 
+  for ( var k = 0; k < arr.length; k++){
+    var pairing_arr = [];
+    pairing_arr.push(arr[k]);
+    var temp = arr.filter(function(x){
+       return arr[k] === x;
+    });
+    pairing_arr.push(temp.length);
+    arr = arr.filter(function(x){
+       return arr[k] !== x;
+    });
+    holding_arr.push(pairing_arr);
+    k--;
+  }
+  return holding_arr;
+}

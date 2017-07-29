1d278add95c8ec2f014275109c986b4c180e085c
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:32:07 -0600

    Update SumAll.js

--- Intermediate Algorithm Scripting/SumAll.js
+++ Intermediate Algorithm Scripting/SumAll.js
@@ -1,3 +1,12 @@
+
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+The lowest number will not always come first.
+
 function sumAll(arr) {
   var max = Math.max(arr[0], arr[1]);
   var min = Math.min(arr[0], arr[1]);

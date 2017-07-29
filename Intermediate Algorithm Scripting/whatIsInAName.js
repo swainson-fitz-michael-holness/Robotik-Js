562b3bb35d601debf2dcab98698abbaf852a0d1d
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:41:17 -0600

    Update whatIsInAName.js

--- Intermediate Algorithm Scripting/whatIsInAName.js
+++ Intermediate Algorithm Scripting/whatIsInAName.js
@@ -1,3 +1,11 @@
+
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). 
+// Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
 function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];

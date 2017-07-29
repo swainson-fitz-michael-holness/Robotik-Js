a64a3818afaf7ccba81e232b72044cb09dfd1a22
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:32:53 -0600

    Create diffArray.js

---
+++ Intermediate Algorithm Scripting/diffArray.js
@@ -0,0 +1,30 @@
+function diffArray(arr1, arr2) {
+  var temp = [];
+  var newArr = [];
+  
+  for(var i = 0; i < arr1.length; i++) {
+    for(var j = 0; j < arr2.length; j++) {
+      if(arr1[i] === arr2[j]) {
+       temp = arr2.splice(arr2.indexOf(arr2[j]), 1).concat(temp);
+      } 
+    }
+  }
+
+  for(var m = 0; m < temp.length; m++) {
+    for(var n = 0; n < arr1.length; n++) {
+      if(temp[m] === arr1[n]) {
+        arr1.splice(arr1.indexOf(arr1[n]), 1);
+      } 
+    }
+  }
+  
+  newArr = arr1.concat(arr2);
+  
+  console.log(temp);
+  console.log(arr2);
+  console.log(arr1);
+  console.log(newArr);
+  return newArr;
+}
+
+diffArray( [1, 7, 3, 4], ["man", 1, 7, 3, "piglet"]);

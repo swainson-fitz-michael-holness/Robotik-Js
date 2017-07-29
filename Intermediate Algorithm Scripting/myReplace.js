d4cbcd8e09957780895114549ff43565b4343254
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:44:09 -0600

    Update myReplace.js

--- Intermediate Algorithm Scripting/myReplace.js
+++ Intermediate Algorithm Scripting/myReplace.js
@@ -2,7 +2,12 @@
 // Contact - swainson.holness@gmail.com
 
 // FCC Description:
-// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+// First argument is the sentence to perform the search and replace on.
+// Second argument is the word that you will be replacing (before).
+// Third argument is what you will be replacing the second argument with (after).
+// NOTE: Preserve the case of the original word when you are replacing it. 
+// For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
 
 function myReplace(str, before, after) {
   var sentence;

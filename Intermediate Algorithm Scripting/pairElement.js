d10c687499373653094c53fa13c5aa5c79c924cf
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:48:13 -0600

    Update pairElement.js

--- Intermediate Algorithm Scripting/pairElement.js
+++ Intermediate Algorithm Scripting/pairElement.js
@@ -1,3 +1,11 @@
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+// Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+// Return the provided character as the first element in each array.
+
 function pairElement(str) {
   var Arr = [];
   var DNA = str.split("");

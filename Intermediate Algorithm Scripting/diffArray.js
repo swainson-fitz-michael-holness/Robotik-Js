c3a3892298825bc50187c620ee24ff008a9a21e9
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:34:31 -0600

    Update diffArray.js

--- Intermediate Algorithm Scripting/diffArray.js
+++ Intermediate Algorithm Scripting/diffArray.js
@@ -2,7 +2,8 @@
 // Contact - swainson.holness@gmail.com
 
 // FCC Description:
-// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+// In other words, return the symmetric difference of the two arrays.
 
 function diffArray(arr1, arr2) {
   var temp = [];

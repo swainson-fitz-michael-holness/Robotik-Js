6e3d036ee9ea78324972c95a25f8e5de01cbf0f0
Swainson Holness <swainson.holness@gmail.com>
2017-07-28 23:45:48 -0600

    Update translatePigLatin.js

--- Intermediate Algorithm Scripting/translatePigLatin.js
+++ Intermediate Algorithm Scripting/translatePigLatin.js
@@ -1,3 +1,9 @@
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
 function translatePigLatin(str) {
   var vowel = [
     'a',

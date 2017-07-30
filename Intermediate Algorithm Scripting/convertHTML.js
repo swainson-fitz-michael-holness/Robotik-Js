748940780a4c7a7b4086c32a5f855b487d7c85c6
Swainson Holness <swainson.holness@gmail.com>
2017-07-29 22:20:40 -0600

    Update convertHTML.js

--- Intermediate Algorithm Scripting/convertHTML.js
+++ Intermediate Algorithm Scripting/convertHTML.js
@@ -1,3 +1,9 @@
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
 function convertHTML(str) {
   str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;" ).replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
   return str;

e9a1cc57035041b6f06ebe18fc2cc320aa202ff0
Swainson Holness <swainson.holness@gmail.com>
2017-07-29 22:18:41 -0600

    Create convertHTML.js

---
+++ Intermediate Algorithm Scripting/convertHTML.js
@@ -0,0 +1,6 @@
+function convertHTML(str) {
+  str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;" ).replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");

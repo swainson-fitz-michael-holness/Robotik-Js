69601188867df7335928c4d0358bdf9324fcf4f4
Swainson Holness <swainson.holness@gmail.com>
2017-07-29 22:05:55 -0600

    Update booWho.js

--- Intermediate Algorithm Scripting/booWho.js
+++ Intermediate Algorithm Scripting/booWho.js
@@ -1,3 +1,9 @@
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
 function booWho(bool) {
   if(typeof bool === "boolean"){
     return true;

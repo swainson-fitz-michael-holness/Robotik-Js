2babdea93a2aeaafcf8151030692e1c9b131d3e5
Swainson Holness <swainson.holness@gmail.com>
2017-07-29 22:24:28 -0600

    Update spinalCase.js

--- Intermediate Algorithm Scripting/spinalCase.js
+++ Intermediate Algorithm Scripting/spinalCase.js
@@ -1,3 +1,9 @@
+// Author - Swainson Holness
+// Contact - swainson.holness@gmail.com
+
+// FCC Description:
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins

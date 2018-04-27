4742dfce91eebc41ad6dcdd864d363b53735ea87
Swainson Holness <swainson.holness@gmail.com>
2018-04-26 22:54:30 -0600

    Update sumOfTwo.js

--- main-algorithms/sum-of-two/sumOfTwo.js
+++ main-algorithms/sum-of-two/sumOfTwo.js
@@ -17,21 +17,30 @@
 //     return false;
 // }
 
+// function sumOfTwo(a, b, v) {
+//     for(let x = 0; x < a.length; x++) {
+//         b.push(v-a[x]);
+//     }
+//     return !([...Array.from(new Set(b))].length === b.length);
+// }
+
+// function sumOfTwo(a, b, v) {
+//     for(let x = 0; x < a.length; x++) {
+//         b.push(v-a[x]);
+//         if([...Array.from(new Set(b))].length < b.length) {
+//             return true;
+//         } else {
+//             b.pop();
+//         }
+//     }
+//     return false;
+// }
+
 function sumOfTwo(a, b, v) {
-    for(let x = 0; x < a.length; x++) {
+    b = [...Array.from(new Set(b))];
+    a = [...Array.from(new Set(a))];
+    for(let x = 0; x < a.length; x++) {  
         b.push(v-a[x]);
     }
     return !([...Array.from(new Set(b))].length === b.length);
-}
-
-function sumOfTwo(a, b, v) {
-    for(let x = 0; x < a.length; x++) {
-        b.push(v-a[x]);
-        if([...Array.from(new Set(b))].length < b.length) {
-            return true;
-        } else {
-            b.pop();
-        }
-    }
-    return false;
 }

1a176002feade7e3812db0d2ff4c9f67cab322ab
Swainson Holness <swainson.holness@gmail.com>
2018-04-26 22:45:54 -0600

    Create sumOfTwo.js

---
+++ main-algorithms/sum-of-two/sumOfTwo.js
@@ -0,0 +1,37 @@
+// function sumOfTwo(a, b, v) {
+//     for(let x = 0; x < a.length; x++) {
+//         if (b.includes(Math.abs(a[x] - v))){
+//             return true;
+//         } 
+//     }
+//     return false;
+// }
+
+
+// function sumOfTwo(a, b, v) {
+//     for(let x = 0; x < a.length; x++) {
+//         if(b.findIndex(function(e) {return e === Math.abs(a[x] - v)}) > -1) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+function sumOfTwo(a, b, v) {
+    for(let x = 0; x < a.length; x++) {
+        b.push(v-a[x]);
+    }
+    return !([...Array.from(new Set(b))].length === b.length);
+}
+
+function sumOfTwo(a, b, v) {
+    for(let x = 0; x < a.length; x++) {
+        b.push(v-a[x]);
+        if([...Array.from(new Set(b))].length < b.length) {
+            return true;
+        } else {
+            b.pop();
+        }
+    }
+    return false;
+}

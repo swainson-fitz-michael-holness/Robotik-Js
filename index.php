76ade81aa85d0ce87c01ed99b56a251a1e4611de
Swainson Holness <swainson.holness@gmail.com>
2019-12-18 12:41:43 -0700

    php update

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Robotic.js</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+</head>
+
+<body>
+	<h1>Hello</h1>
+  <script src="./archive/main.js"></script>
+</body>
+</html>
